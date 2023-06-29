3e131eddbe1cb19b0ac1d5c93c99c1dfbafba5f5
Cristian Zambrano <cristian@zamcham.com>
2023-06-29 08:32:49 -0400

    Update decorator.rb

--- decorator.rb
+++ decorator.rb
@@ -13,14 +13,3 @@ class Decorator < Nameable
   end
 end
 
-class CapitalizeDecorator < Decorator
-  def correct_name
-    @nameable.correct_name.capitalize
-  end
-end
-
-class TrimmerDecorator < Decorator
-  def correct_name
-    @nameable.correct_name.slice(0, 10)
-  end
-end
