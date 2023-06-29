60a437006172aa226070a1e55b0628f9799c1814
Cristian Zambrano <cristian@zamcham.com>
2023-06-29 12:46:42 -0400

    Update student.rb to include classroom class

--- student.rb
+++ student.rb
@@ -1,11 +1,19 @@
 require_relative 'person'
+require_relative 'classroom'
 
 class Student < Person
+  attr_reader :classroom
+  
   def initialize(id, classroom, name: 'Unknown', age: nil, parent_permission: true)
     super(id, name, age, parent_permission)
     @classroom = classroom
   end
 
+  def set_classroom(classroom)
+    @classroom = classroom
+    classroom.students << self
+  end
+  
   def play_hooky
     '¯\\(ツ)/¯'
   end
