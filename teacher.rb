6f6c6bb3b35d4d1db9307223c772e3188d8d951e
Cristian Zambrano <cristian@zamcham.com>
2023-06-29 18:48:51 -0400

    Update teacher.rb parameters in initialize method

--- teacher.rb
+++ teacher.rb
@@ -2,7 +2,7 @@ require_relative 'person'
 
 class Teacher < Person
   def initialize(id, specialization, name: 'Unknown', age: nil, parent_permission: true)
-    super(id, name, age, parent_permission)
+    super(id, name: name, age: age, parent_permission: parent_permission)
     @specialization = specialization
   end
 
