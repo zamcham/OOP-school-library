cf756623cbf0ec36f5fa7f32882b4d63aafb6528
Cristian Zambrano <cristian@zamcham.com>
2023-06-29 15:40:49 -0400

    Update person.rb rental method to include book
    Co-authored-by: Abraham Ibizugbe <abrahamibizugbe@gmail.com>

--- person.rb
+++ person.rb
@@ -25,10 +25,10 @@ class Person < Nameable
     name
   end
 
-  def add_rental(person, date)
-    Rental.new(date, self, person)
+  def add_rental(book, date)
+    Rental.new(date, self, book)
   end
-
+  
   private
 
   def of_age?
