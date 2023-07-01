8ab1931ad5045c43974b26389936d6bfca817745
Cristian Zambrano <cristian@zamcham.com>
2023-07-01 09:54:00 -0400

    Update rental.rb

--- rental.rb
+++ rental.rb
@@ -1,10 +1,10 @@
 class Rental
   attr_accessor :date, :book, :person
 
-  def initialize(date, book, person)
-    @date = date
+  def initialize(book, person, date)
     @book = book
     @person = person
+    @date = date
     book.rentals << self
     person.rentals << self
   end
