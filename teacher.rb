55582022189458fa68a9c442bafee79fb54db272
Cristian Zambrano <cristian@zamcham.com>
2023-07-01 09:55:00 -0400

    refactor teacher.rb

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,8 @@
 require_relative 'person'
 
 class Teacher < Person
-  def initialize(id, specialization, name: 'Unknown', age: nil, parent_permission: true)
-    super(id, name: name, age: age, parent_permission: parent_permission)
+  def initialize(age, specialization, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
