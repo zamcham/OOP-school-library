9ed16ca7fa2914f10500d77104b2fb956a3b1f80
Cristian Zambrano <cristian@zamcham.com>
2023-07-06 13:59:31 -0400

    add missing attr

--- person.rb
+++ person.rb
@@ -2,7 +2,7 @@ require_relative 'decorator'
 require_relative 'nameable'
 
 class Person < Nameable
-  attr_accessor :name, :age, :rentals
+  attr_accessor :name, :age, :rentals, :parent_permission
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
