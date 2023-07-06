7a2e01ad9781563b00052aa2285462cb7464735d
zamcham <cristian@zamcham.com>
2023-07-06 16:55:56 -0400

    add id parameter

--- person.rb
+++ person.rb
@@ -5,9 +5,9 @@ class Person < Nameable
   attr_accessor :name, :age, :rentals, :parent_permission
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true, id: nil)
     super()
-    @id = rand(1..1000)
+    @id = id || rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
