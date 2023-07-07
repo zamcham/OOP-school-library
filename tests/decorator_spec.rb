8a5ce714bb63a3176c53111dccbef7dd143ebaa3
Cristian Zambrano <criszamcham@gmail.com>
2023-07-07 18:32:14 -0400

    add decorator test

---
+++ tests/decorator_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../nameable'
+require_relative '../decorator'
+require 'rspec'
+
+class TestNameable
+  def correct_name
+    'Test Name'
+  end
+end
+
+RSpec.describe Decorator do
+  let(:nameable) { TestNameable.new }
+  let(:decorator) { Decorator.new(nameable) }
+
+  describe '#correct_name' do
+    it 'returns the correct name using the nameable object' do
+      expect(decorator.correct_name).to eq('Test Name')
+    end
+  end
+end
